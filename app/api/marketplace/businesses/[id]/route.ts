701f1afea68265fe060bfc93f9b2e54b53a8958f
anas <anassayyed000@gmail.com>
2025-12-16 10:46:53 +0530

    view profile

--- app/api/marketplace/businesses/[id]/route.ts
+++ app/api/marketplace/businesses/[id]/route.ts
@@ -1,8 +1,15 @@
 import { NextResponse } from "next/server";
 import { businesses } from "@/lib/recodd/mockData";
 
-export async function GET(_: Request, { params }: { params: { id: string } }) {
-  const profile = businesses.find(b => b.id === Number(params.id));
+export async function GET(
+  _req: Request, 
+  context : { params: Promise <{ id: string }> }
+) {
+
+  const {id} = await context.params;
+
+  const numericId = Number(id);
+  const profile = businesses.find(b => b.id === numericId);
 
   if (!profile) {
     return NextResponse.json({ message: "Not found" }, { status: 404 });
--- app/api/marketplace/freelancers/[id]/route.ts
+++ app/api/marketplace/freelancers/[id]/route.ts
@@ -3,11 +3,13 @@ import { freelancers } from "@/lib/recodd/mockData";
 
 export async function GET(
   _req: Request,
-  { params }: { params: { id: string } }
+  //`params` is a Promise and must be unwrapped with await or React.use()
+  context: { params: Promise<{ id: string }> }
 ) {
-  const id = Number(params.id);
+  const { id } = await context.params;
 
-  const profile = freelancers.find(f => f.id === id);
+  const numericId = Number(id);
+  const profile = freelancers.find(f => f.id === numericId);
 
   if (!profile) {
     return NextResponse.json({ message: "Profile not found" }, { status: 404 });
