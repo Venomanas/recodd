d00bd8583cba5a1bf9edd16d128e887a9238f447
anas <anassayyed000@gmail.com>
2025-12-20 20:23:03 +0530

    models set

--- app/components/LayoutContainer.tsx
+++ app/components/LayoutContainer.tsx
@@ -3,7 +3,7 @@ export const LayoutContainer = ({
 }: {
   children: React.ReactNode;
 }) => (
-  <div className="w-full max-w-7xl mx-auto px-4 md:px-10 lg:px-20">
+  <div className="w-full max-w-7xl mx-auto px-4 md:px-8 lg:px-20">
     {children}
   </div>
 );
